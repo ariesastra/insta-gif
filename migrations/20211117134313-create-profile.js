a65e648b6701e711b49247d74a66f01082515936
Agnes Suryani <el.agnesurya@gmail.com>
2021-11-17 21:01:36 +0700

    setup

---
+++ migrations/20211117134313-create-profile.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up:  (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      age: {
+        type: Sequelize.INTEGER
+      },
+      profilepic: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down:  (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Profiles');
+  }
+};
