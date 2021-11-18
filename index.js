df11b0605a13aa59ebee498a6b30572d35a32ac8
Arie Sastra Hadiprawira <ariesastra@windowslive.com>
2021-11-19 03:24:50 +0700

    Update port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require('express-session')
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 // view engine
