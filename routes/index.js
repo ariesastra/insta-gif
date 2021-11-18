e9bcda676179d6dd0a4a0ce79b1aedd73e84f9a0
Agnes Suryani <el.agnesurya@gmail.com>
2021-11-18 19:53:40 +0700

    logout

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,35 @@ const LoginController = require('../controllers/loginController');
 const RegisterController = require('../controllers/registerController');
 const ProfileController = require('../controllers/profileController');
 
-route.get('/', IndexController.getInstaGif);
-route.get('/post', IndexController.postInstaGif);
-// route.post('/post', IndexController.postInstaGif);
-// route.get('/post/edit/:id', IndexController.postInstaGif);
-// route.post('/post/edit/:id', IndexController.postInstaGif);
-// route.get('/post/delete/:id', IndexController.postInstaGif);
 route.get('/login', LoginController.getLoginPage);
 route.post('/login', LoginController.postLogin);
 route.get('/register', RegisterController.getRegisterPage);
 route.post('/register', RegisterController.postRegister);
+
+
+route.use(function (req, res, next) {
+    //console.log(req.session)
+    if (!req.session.userId) {
+        const error = `You need to login!`
+        res.redirect(`/login?error=${error}`)
+    } else {
+        next()
+    }
+})
+
+route.get('/', IndexController.getInstaGif);
+route.get('/post', IndexController.getAddInstaGif);
+route.post('/post', IndexController.getPostInstaGif);
+// route.get('/post/edit/:id', IndexController.postInstaGif);
+// route.post('/post/edit/:id', IndexController.postInstaGif);
+// route.get('/post/delete/:id', IndexController.postInstaGif);
 route.get('/profile', ProfileController.getProfilePage);
 route.get('/profile/edit', ProfileController.getEditProfile);
 route.post('/profile/edit', ProfileController.postEditProfile);
 
-route.use(function (req, res, next) {
-  console.log(req.session)
-  if (!req.session.userId) {
-    const error = `You need to login!`
-    res.redirect(`/login?error=${error}`)
-  } else {
-    next()
-  }
-})
+route.get('/logout',LoginController.getLogOut)
 
-route.get('/', IndexController.getInstaGif);
+
+
 
 module.exports = route;
