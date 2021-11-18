ba56c733b131dcd652b9776183398fe41c0b71c1
ariesastra <ariesastra@windowslive.com>
2021-11-18 20:07:35 +0700

    push lagi

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,52 +1,52 @@
-const {User} = require('../models')
+const { User } = require('../models')
 const bcrypt = require('bcryptjs');
 
-class LoginController{
-     static  getLoginPage(req,res){
-         const {error} = req.query
+class LoginController {
+    static getLoginPage(req, res) {
+        const { error } = req.query
         //  console.log(req.query);
-         res.render('auth-pages/loginform', {err : error})
-     }
+        res.render('auth-pages/loginform', { err: error })
+    }
 
-     static postLogin(req,res){
-        const {email, password} = req.body
-         User.findOne({
-             where :{
-                 email
-             }
-         })
-         .then(user=>{
-            //  console.log(user)
-            if (user) {
-                let validPassword = bcrypt.compareSync(password, user.password)
-                
-                if (validPassword) {
-                    req.session.userId = user.id
+    static postLogin(req, res) {
+        const { email, password } = req.body
+        User.findOne({
+            where: {
+                email
+            }
+        })
+            .then(user => {
+                //  console.log(user)
+                if (user) {
+                    let validPassword = bcrypt.compareSync(password, user.password)
 
-                    return res.redirect('/')
+                    if (validPassword) {
+                        req.session.userId = user.id
+
+                        return res.redirect('/')
+                    } else {
+                        const errmessage = (`Invalid Password`)
+                        return res.redirect(`/login?error=${errmessage}`)
+                    }
                 } else {
-                    const errmessage = (`Invalid Password`)
+                    const errmessage = (`Invalid Email & Password`)
                     return res.redirect(`/login?error=${errmessage}`)
                 }
-            } else {
-                const errmessage = (`Invalid Email & Password`)
-                return res.redirect(`/login?error=${errmessage}`)
-            }
-         })
-         .catch(err=>{
-             res.send(err)
-         })
-     }
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
 
-     static getLogOut(req,res){
-         req.session.destroy((err)=>{
-             if (err) {
-                 res.send(err)
-             } else {
-                 res.redirect('/login')
-             }
-         })
-     }
+    static getLogOut(req, res) {
+        req.session.destroy((err) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.redirect('/login')
+            }
+        })
+    }
 }
 
-module.exports =LoginController
+module.exports = LoginController
--- routes/index.js
+++ routes/index.js
@@ -8,51 +8,26 @@ route.get('/login', LoginController.getLoginPage);
 route.post('/login', LoginController.postLogin);
 route.get('/register', RegisterController.getRegisterPage);
 route.post('/register', RegisterController.postRegister);
-<<<<<<< HEAD
+route.get('/logout', LoginController.getLogOut)
 
 route.use(function (req, res, next) {
-  console.log(req.session)
+  //console.log(req.session)
   if (!req.session.userId) {
     const error = `You need to login!`
     res.redirect(`/login?error=${error}`)
   } else {
     next()
   }
-});
-
-route.get('/', IndexController.getInstaGif);
-route.get('/post', IndexController.postGifPage);
-route.post('/post', IndexController.postInstaGif);
-=======
-
-
-route.use(function (req, res, next) {
-    //console.log(req.session)
-    if (!req.session.userId) {
-        const error = `You need to login!`
-        res.redirect(`/login?error=${error}`)
-    } else {
-        next()
-    }
 })
 
 route.get('/', IndexController.getInstaGif);
 route.get('/post', IndexController.getAddInstaGif);
 route.post('/post', IndexController.getPostInstaGif);
->>>>>>> e9bcda676179d6dd0a4a0ce79b1aedd73e84f9a0
 // route.get('/post/edit/:id', IndexController.postInstaGif);
 // route.post('/post/edit/:id', IndexController.postInstaGif);
 // route.get('/post/delete/:id', IndexController.postInstaGif);
 route.get('/profile', ProfileController.getProfilePage);
 route.get('/profile/edit', ProfileController.getEditProfile);
 route.post('/profile/edit', ProfileController.postEditProfile);
-
-<<<<<<< HEAD
-=======
-route.get('/logout',LoginController.getLogOut)
-
-
-
->>>>>>> e9bcda676179d6dd0a4a0ce79b1aedd73e84f9a0
 
 module.exports = route;
