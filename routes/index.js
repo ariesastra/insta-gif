1e7fb55c555c82066a52323401afc11228ba7a32
Agnes Suryani <el.agnesurya@gmail.com>
2021-11-18 09:29:49 +0700

    register

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const route = require('express').Router();
-const IndexController = require('../controller');
-const LoginController = require('../controller/loginController');
-const RegisterController = require('../controller/registerController');
-const ProfileController = require('../controller/profileController');
+const IndexController = require('../controllers');
+const LoginController = require('../controllers/loginController');
+const RegisterController = require('../controllers/registerController');
+const ProfileController = require('../controllers/profileController');
 
 route.get('/', IndexController.getInstaGif);
 // route.post('/post', IndexController.postInstaGif);
-// route.get('/login', LoginController.getLoginPage);
+route.get('/login', LoginController.getLoginPage);
 // route.post('/login', LoginController.postLogin);
-// route.get('/register', RegisterController.getRegisterPage);
-// route.post('/register', RegisterController.postRegister);
+route.get('/register', RegisterController.getRegisterPage);
+route.post('/register', RegisterController.postRegister);
 // route.get('/profile', ProfileController.getProfilePage);
 // route.get('/profile/edit', ProfileController.getEditProfile);
 // route.post('/profile/edit', ProfileController.postEditProfile);
